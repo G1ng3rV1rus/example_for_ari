9400801a49ecf40363f79065b6fc933efe5f559c
erulabs <seandon.mooy@gmail.com>
2016-12-01 21:05:14 -0800

    force input to be read as a FLOATing point number

--- ari.py
+++ ari.py
@@ -1,15 +1,15 @@
 print("Please enter your income for the following months:")
-income10 = input("October:")
-income11 = input("November:")
-income12 = input("December:")
+income10 = float(input("October:"))
+income11 = float(input("November:"))
+income12 = float(input("December:"))
 total_income = income10+income11+income12
 ave_income = total_income/3
 print('Your average monthly income is', ave_income, 'dollar(s)')
 
 print("Now enter your total expenses for the following months:")
-exp10 = input("October:")
-exp11 = input("November:")
-exp12 = input("December:")
+exp10 = float(input("October:"))
+exp11 = float(input("November:"))
+exp12 = float(input("December:"))
 total_expense = exp10+exp11+exp12
 ave_exp = total_expense/3
 print('Your monthly expenses seem to cost you an average of', ave_exp, 'dollar(s)')
