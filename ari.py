6e43dc8a2a0435e24d182c01d2e7392f8a4d9a29
erulabs <seandon.mooy@gmail.com>
2016-12-01 21:00:56 -0800

    fix print syntax

--- ari.py
+++ ari.py
@@ -4,7 +4,7 @@ income11 = input("November:")
 income12 = input("December:")
 total_income = income10+income11+income12
 ave_income = total_income/3
-print'Your average monthly income is', ave_income, 'dollar(s)'
+print('Your average monthly income is', ave_income, 'dollar(s)')
 
 print("Now enter your total expenses for the following months:")
 exp10 = input("October:")
@@ -12,6 +12,6 @@ exp11 = input("November:")
 exp12 = input("December:")
 total_expense = exp10+exp11+exp12
 ave_exp = total_expense/3
-print 'Your monthly expenses seem to cost you an average of', ave_exp, 'dollar(s)'
+print('Your monthly expenses seem to cost you an average of', ave_exp, 'dollar(s)')
 
-print 'Your average monthly gain/loss is', ave_income-ave_exp, 'dollar(s)'
+print('Your average monthly gain/loss is', ave_income-ave_exp, 'dollar(s)')
